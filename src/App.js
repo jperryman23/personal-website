a1dd36662845491d99c945544d1d7c93cfe8d46c
Jules Perryman <jules.perryman@gmail.com>
2017-10-03 17:28:15 -0600

    setting up menu bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Jules Perryman Web Developer</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+
+                    <div className="App-title">Jules Perryman | Full Stack Web Developer
+                    </div>
+
+                    <div className="App-menu">
+                        <div>About</div>
+                        <div>Portfolio</div>
+                        <div>Resume</div>
+                        <div>Contact</div>
+                    </div>
+                </header>
+                <p className="App-intro"></p>
+            </div>
+        );
+    }
 }
 
 export default App;
