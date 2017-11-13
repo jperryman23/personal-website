ff9c3cf9a9f7e90cc9c1296eaeddaf985c8be07c
Jules Perryman <jules.perryman@gmail.com>
2017-11-13 16:43:52 -0700

    menu item updated

--- src/App.js
+++ src/App.js
@@ -5,23 +5,36 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
+            <div className='App'>
+                <Header />
 
-                    <div className="App-title">Jules Perryman | Full Stack Web Developer
-                    </div>
-
-                    <div className="App-menu">
-                        <div className="Menu-item">About</div>
-                        <div className="Menu-item">Portfolio</div>
-                        <div className="Menu-item">Resume</div>
-                        <div className="Menu-item">Contact</div>
-                    </div>
-                </header>
-                <p className="App-intro"></p>
             </div>
+
         );
     }
 }
+
+const Header = () =>
+<div>
+    <header className="App-header">
+        <div className="App-title">
+            <div className="Title-item">
+                <img src="/../../images/logo-white.png" alt="" height="35px"/>
+            </div>
+            <div className="Title-item">J u l e s</div>
+            <div className="Title-item">P e r r y m a n </div>
+            {/* <div className="Title-item">| Full Stack Web Developer</div> */}
+        </div>
+
+        <div className="App-menu">
+            <a className="Menu-item" href='#about'>About</a>
+            <a className="Menu-item" href='#resume'>Resume</a>
+            <a className="Menu-item" href='#portfolio'>Portfolio</a>
+            <a className="Menu-item" href='#contact'>Contact</a>
+        </div>
+
+    </header>
+</div>
+
 
 export default App;
