1bb79023d69046b0d88a9390ba5be342113c7580
Jules Perryman <jules.perryman@gmail.com>
2017-12-13 20:10:47 -0700

    completed about tab

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,7 @@ export default class About extends Component {
             <section className="AboutSection">
 
                 <div>
+                    
                     <p>I am a full stack web developer with a business background.</p>
 
                     <p>I am interested in both front and back-end web development positions as well as cryptocurrencies and blockchain technology.</p>
@@ -27,7 +28,20 @@ export default class About extends Component {
                     <p>I am a quick learner and I particularly excel at problem solving, maximizing efficiency, and creative solutions. </p>
 
                     <p>As an avid world traveler and philanthropist I love experiencing new countries and cultures.</p>
+
+                <div className="galvanizeLogo">
+
+                    <div className="aboutMore">For more about me visit my personal website → <u>
+                    <a href="https://www.julesperryman.com" target="_blank"> www.julesperryman.com </a> </u> </div>
+
+                    <img className="g46logo"  alt="logo" src='../images/g46logo.png'/>
+
+
                 </div>
+
+                </div>
+
+
 
             </section>
 
